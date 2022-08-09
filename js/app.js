36fe5288bcac036046ab24e625adb3d2b18f5b6a
Faruqe99 <106181502+Faruqe99@users.noreply.github.com>
2022-08-09 15:27:25 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,92 @@
+
+var num = 73;
+
+ if (num >= 80 && num <= 100 && num%2 == 0 ) {
+     document.write('A+')
+     document.write('<br>')
+     document.write('Evan')
+    
+
+}  else if  (num >= 80 && num <= 100 && num%2 != 0 ) {
+    document.write('A+')
+    document.write('<br>')
+    document.write('Odd')
+   
+
+    
+}   else if  (num >= 70 && num < 80 && num%2 == 0 ) {
+    document.write('A')
+    document.write('<br>')
+    document.write('Evan')
+   
+
+}   else if  (num >= 70 && num < 80 && num%2 != 0 ) {
+    document.write('A')
+    document.write('<br>')
+    document.write('Odd')
+   
+
+}  else if  (num >= 60 && num < 70 && num%2 == 0 ) {
+    document.write('A-')
+    document.write('<br>')
+    document.write('Evan')
+   
+
+}  else if  (num >= 60 && num < 70 && num%2 != 0 ) {
+    document.write('A-')
+    document.write('<br>')
+    document.write('Odd')
+
+} else if  (num >= 50 && num < 60 && num%2 == 0 ) {
+    document.write('B')
+    document.write('<br>')
+    document.write('Evan')
+   
+
+}   else if  (num >= 50 && num < 60 && num%2 != 0 ) {
+    document.write('B')
+    document.write('<br>')
+    document.write('Odd')
+   
+
+} else if  (num >= 40 && num < 50 && num%2 == 0 ) {
+    document.write('C')
+    document.write('<br>')
+    document.write('Evan')
+   
+
+} else if  (num >= 40 && num < 50 && num%2 != 0 ) {
+    document.write('C')
+    document.write('<br>')
+    document.write('Odd')
+   
+
+} else if  (num >= 33 && num < 40 && num%2 == 0 ) {
+    document.write('D')
+    document.write('<br>')
+    document.write('Evan')
+   
+
+}
+else if  (num >= 33 && num < 40 && num%2 != 0 ) {
+    document.write('D')
+    document.write('<br>')
+    document.write('Odd')
+   
+
+} else if  (num >=1 && num < 33 && num%2 == 0 ) {
+    document.write('F')
+    document.write('<br>')
+    document.write('Evan')
+   
+
+} else if  (num >=1 && num < 33 && num%2 != 0 ) {
+    document.write('F')
+    document.write('<br>')
+    document.write('Odd')
+   
+
+}
+
+
+
